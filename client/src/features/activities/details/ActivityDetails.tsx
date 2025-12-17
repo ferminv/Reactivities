63ccb550c196a50c4f1012fff7e9b7270d36964b
Fermin Velez <ferminvelez3@gmail.com>
2025-12-17 16:38:38 -0300

    React Router routing

--- client/src/features/activities/details/ActivityDetails.tsx
+++ client/src/features/activities/details/ActivityDetails.tsx
@@ -1,17 +1,15 @@
 import { Button, Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material"
-import { useActivities } from "../../../lib/hooks/useActivities"
+import { Link, useNavigate, useParams } from "react-router"
+import { useActivities } from "../../../lib/hooks/useActivities";
 
-type Props = {
-    selectedActivity: Activity
-    cancelSelectActivity: () => void
-    openForm: (id: string) => void
-}
+export default function ActivityDetails() {
+    const navigate = useNavigate();
+    const {id} = useParams(); //id or any param has to match the one we used on the route declared on our browser router on Routes.tsx
+    const {activity, isLoadingActivity} = useActivities(id);
 
-export default function ActivityDetails({selectedActivity, cancelSelectActivity, openForm} : Props) {
-    const {activities} = useActivities();
-    const activity = activities?.find(x => x.id === selectedActivity.id)
+    if (isLoadingActivity) return <Typography>Loading...</Typography>
 
-    if (!activity) return <Typography>Loading...</Typography>
+    if (!activity) return <Typography>Activity not found</Typography>
   
     return (
     <Card sx={{borderRadius: 3}}>
@@ -24,8 +22,10 @@ export default function ActivityDetails({selectedActivity, cancelSelectActivity,
             <Typography variant="body1">{activity.description}</Typography>
         </CardContent>
         <CardActions>
-            <Button onClick={() => openForm(activity.id)} color="primary">Edit</Button>
-            <Button onClick={cancelSelectActivity} color="inherit">Cancel</Button>
+            {/* MANERA VIEJA Y DOS MANERAS DE NAVEGAR CON ROUTING REACT ROUTER */}
+            {/* <Button onClick={() => openForm(activity.id)} color="primary">Edit</Button> */}
+            <Button component={Link} to={`/manage/${activity.id}`} color="primary">Edit</Button>
+            <Button onClick={() => navigate('/activities')} color="inherit">Cancel</Button>
         </CardActions>
     </Card>
   )
